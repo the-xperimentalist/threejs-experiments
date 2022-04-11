6da00bae71e57cd8c259e7f8cf26750eb22526b0
Mayank Prasoon <mayank.prasoon@outlook.com>
2022-04-11 20:13:23 +0530

    Included initial code

--- experiment2/src/script.js
+++ experiment2/src/script.js
@@ -1,1 +1,14 @@
 import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+// We can use wave equation for fun experimental waves: y=A*sin(wt+ya)
+
+/*
+ * Completing basic setup including GUI controls, canvas, scene and axes helpers
+ */
+const gui = new dat.GUI()
+const canvas = document.querySelector("canvas.webgl")
+const scene = new THREE.Scene()
+const axesHelper = new THREE.AxesHelper()
